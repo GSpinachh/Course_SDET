e5bd40a982c22aa700b47444150663580aab8160
GSpinachh <darpopova13@gmail.com>
2024-10-30 20:33:00 +0400

    Переделала 2 задание с учётом правок и предложений

--- oop.py
+++ oop.py
@@ -13,12 +13,28 @@ class CelestialBody(ABC):
     def describe(self):
         pass  
 
-    @classmethod
-    def get_celestial_body_count(cls):
-        return cls.count 
+    @staticmethod
+    def get_celestial_body_count() -> int:
+        return CelestialBody.count 
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        self._name = name
+
+    @property
+    def mass(self) -> float:
+        return self._mass
+
+    @name.setter
+    def mass(self, mass: float):
+        self._mass = mass
 
     def get_info(self):
-        return f"Имя: {self._name}, Масса: {self._mass} солнечная масса"
+        return f"Имя: {self.name}, Масса: {self.mass} солнечная масса"
 
 
 class Star(CelestialBody):
@@ -27,11 +43,16 @@ class Star(CelestialBody):
         self._temperature = temperature  
 
     def describe(self):
-        return f"{self._name} звезда с температурой {self._temperature} K."
+        return f"{self.name} - звезда с температурой {self._temperature} K."
 
     def generate_flare(self):
         flare_strength = random.randint(1, 10)
-        return f"На {self._name} произошла солнечная вспышка с силой {flare_strength}!"
+        if flare_strength <= 3:
+            return f"На {self.name} произошла слабая солнечная вспышка с силой {flare_strength}!"
+        elif flare_strength <= 7:
+            return f"На {self.name} произошла средняя солнечная вспышка с силой {flare_strength}!"
+        else:
+            return f"На {self.name} произошла сильная солнечная вспышка с силой {flare_strength}!"
 
 
 class Planet(CelestialBody):
@@ -40,23 +61,27 @@ class Planet(CelestialBody):
         self._distance_from_star = distance_from_star  
 
     def describe(self):
-        return f"{self._name} планета, расположенная на растоянии {self._distance_from_star} а.е. от своей звезды."
+        return f"{self.name} - планета, расположенная на расстоянии {self._distance_from_star} а.е. от своей звезды."
 
     def calculate_orbit_time(self):
         orbit_period_years = (self._distance_from_star ** 1.5)  
-        return f"{self._name} требуется примерно {orbit_period_years:.2f} лет для совершения одного оборота."
+        return f"{self.name} требуется примерно {orbit_period_years:.2f} лет для совершения одного оборота."
 
-if __name__ == "__main__":
-    star = Star(name="Солнце", mass=1.0, temperature=5772)
-    planet_earth = Planet(name="Земля", mass=1.0, distance_from_star=1.0)
-    planet_mars = Planet(name="Марс", mass=0.107, distance_from_star=1.52)
 
-    print(star.describe())  
-    print(planet_earth.describe()) 
-    print(planet_mars.describe())  
+star = Star(name="Солнце", mass=1.0, temperature=5772)
+planet_earth = Planet(name="Земля", mass=1.0, distance_from_star=1.0)
+planet_mars = Planet(name="Марс", mass=0.107, distance_from_star=1.52)
 
-    print(star.generate_flare())
-    print(planet_earth.calculate_orbit_time())  
-    print(planet_mars.calculate_orbit_time())  
+print(star.describe())  
+print(planet_earth.describe()) 
+print(planet_mars.describe())  
 
-    print(f"Создано небесных тел: {CelestialBody.get_celestial_body_count()}") 
+print(star.generate_flare())
+print(planet_earth.calculate_orbit_time())  
+print(planet_mars.calculate_orbit_time())  
+
+print(f"Создано небесных тел: {CelestialBody.get_celestial_body_count()}")
+
+star.name = "Новое Солнце"
+star.mass = 1.1
+print(star.get_info())  
