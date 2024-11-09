81c26282fe62fe34414c3ddf8dbb809f6c1c7a77
GSpinachh <darpopova13@gmail.com>
2024-11-09 20:42:25 +0400

    Add files via upload

--- filter.py
+++ filter.py
@@ -1,17 +1,14 @@
 import json
 import csv
 
-# Загрузка данных из файла JSON
 with open('in.json', 'r') as file:
     data = json.load(file)
 
-# Генератор для фильтрации пользователей
 def filter(entries):
     for entry in entries:
         if ('+1' in entry['phoneNumber'] or entry['phoneNumber'].startswith('1')) and '4.0 Safari' in entry['userAgent']:
             yield entry
 
-# Сохранение данных в CSV файл
 with open('output.csv', 'w', newline='') as csvfile:
     fieldnames = ['name', 'email', 'address']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
