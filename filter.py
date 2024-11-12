b786996128b4241694472250687337e2985b7f31
GSpinachh <darpopova13@gmail.com>
2024-11-12 18:11:30 +0400

    Add files via upload

--- filter.py
+++ filter.py
@@ -3,6 +3,7 @@ import csv
 import os
 from typing import List, Dict, Any, Generator
 
+
 def read_json_file(json_file: str) -> List[Dict[str, Any]]:
     if not os.path.exists(json_file):
         print("Файл не найден")
@@ -11,10 +12,13 @@ def read_json_file(json_file: str) -> List[Dict[str, Any]]:
     with open(json_file, "r") as file:
         return json.load(file)
 
+
 def filter_data(user_data: List[Dict[str, Any]]) -> Generator[Dict[str, Any], None, None]:
     for user in user_data:
-        if (user["phoneNumber"].startswith("+1") or user["phoneNumber"].startswith("1")) and "4.0 Safari" in user["userAgent"]:
+        if (user["phoneNumber"].startswith("+1") or 
+            user["phoneNumber"].startswith("1")) and "4.0 Safari" in user["userAgent"]:
             yield user
+
 
 def write_csv_file(user_data: Generator[Dict[str, Any], None, None], csv_file: str) -> None:
     fieldnames = ["name", "email", "address"]
@@ -26,14 +30,11 @@ def write_csv_file(user_data: Generator[Dict[str, Any], None, None], csv_file: s
         for data in user_data:
             writer.writerow({name: data[name] for name in fieldnames})
 
+
 def main() -> None:
-
     json_data = read_json_file("in.json")
-    
     filtered_data = filter_data(json_data)
-
     write_csv_file(filtered_data, "output.csv")
-
     print("Данные фильтрации сохранены в новом файле: " "output.csv")
 
 if __name__ == "__main__":
