eff4889150c2d35b9ee17ea393b0896f735aeb78
GSpinachh <darpopova13@gmail.com>
2024-11-12 17:16:09 +0400

    Исправила код с учётом замечаний и предложений

--- filter.py
+++ filter.py
@@ -1,20 +1,40 @@
 import json
 import csv
+import os
+from typing import List, Dict, Any, Generator
 
-with open('in.json', 'r') as file:
-    data = json.load(file)
+def read_json_file(json_file: str) -> List[Dict[str, Any]]:
+    if not os.path.exists(json_file):
+        print("Файл не найден")
+        return []
 
-def filter(entries):
-    for entry in entries:
-        if ('+1' in entry['phoneNumber'] or entry['phoneNumber'].startswith('1')) and '4.0 Safari' in entry['userAgent']:
-            yield entry
+    with open(json_file, "r") as file:
+        return json.load(file)
 
-with open('output.csv', 'w', newline='') as csvfile:
-    fieldnames = ['name', 'email', 'address']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+def filter_data(user_data: List[Dict[str, Any]]) -> Generator[Dict[str, Any], None, None]:
+    for user in user_data:
+        if (user["phoneNumber"].startswith("+1") or user["phoneNumber"].startswith("1")) and "4.0 Safari" in user["userAgent"]:
+            yield user
 
-    writer.writeheader()
-    for user in filter(data):
-        writer.writerow({'name': user['name'], 'email': user['email'], 'address': user['address']})
+def write_csv_file(user_data: Generator[Dict[str, Any], None, None], csv_file: str) -> None:
+    fieldnames = ["name", "email", "address"]
+
+    with open(csv_file, "w", newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+        writer.writeheader()
         
-print("Данные фильтрации сохранены в новом файле: output.csv")
+        for data in user_data:
+            writer.writerow({name: data[name] for name in fieldnames})
+
+def main() -> None:
+
+    json_data = read_json_file("in.json")
+    
+    filtered_data = filter_data(json_data)
+
+    write_csv_file(filtered_data, "output.csv")
+
+    print("Данные фильтрации сохранены в новом файле: " "output.csv")
+
+if __name__ == "__main__":
+    main()
